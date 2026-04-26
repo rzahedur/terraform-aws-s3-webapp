79d4b4f11092acb4a0528e9b11537611e35a0579
Zahedur <Zahedur@DESKTOP-4857TBK>
2026-04-25 20:54:33 -0400

    updated

--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@
 variable "region" {}
 variable "prefix" {}
 variable "name" {}
+
+module "terraform-aws-s3-webapp" {
+  source  = "app.terraform.io/Zenin/terraform-aws-s3-webapp/aws"
+  version = "1.0.0"
+  # insert required variables here
+}
